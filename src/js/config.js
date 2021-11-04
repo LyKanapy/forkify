a9ce0156be6b0f8c8d18d45ef43bc0bbceaa4d08
BigToeEnergy <l.kanapickas@gmail.com>
2021-11-04 21:29:38 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd21df1f0-6714-4d9b-874d-4eac4eba5dad';
+export const MODAL_CLOSE_SEC = 2.5;
