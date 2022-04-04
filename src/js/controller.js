f10cdba091d005b7e2f4131042fc3e47fbe06659
BigToeEnergy <l.kanapickas@gmail.com>
2022-04-04 22:46:35 +0300

    final

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,7 +11,6 @@ import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import addRecipeView from './views/addRecipeView.js';
 
 if (module.hot) {
   module.hot.accept();
